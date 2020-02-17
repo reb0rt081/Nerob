0d0e7519ab862b5315675845e9ab8d941ac622d1
reb0rt081 <ribes.roberto@gmail.com>
2020-02-17 22:20:30 +0100

    Shaping the Client

---
+++ Nerob.Client.Desktop/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Nerob.Client.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Exit += ApplicationOnExit;
+            Startup += ApplicationOnStartup;
+        }
+
+        private void ApplicationOnStartup(object sender, StartupEventArgs e)
+        {
+            ClientBootStrapper clientBootStrapper = new ClientBootStrapper();
+            clientBootStrapper.Run();
+        }
+
+        private void ApplicationOnExit(object sender, ExitEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Nerob.Client.Desktop/ClientBootStrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nerob.Domain;
+using Unity;
+
+namespace Nerob.Client.Desktop
+{
+    public class ClientBootStrapper : IClientBootStrapper
+    {
+        private IUnityContainer container; 
+        public void Run()
+        {
+            CheckUniqueInstanceIsRunning();
+        }
+
+        public void CheckUniqueInstanceIsRunning()
+        {
+            string processName = System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location);
+            if (System.Diagnostics.Process.GetProcessesByName(processName).Count() > 1)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+        }
+
+        public void LoadModules()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
