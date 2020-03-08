50ca17107241e934bf43c96c597c0ecfaaf708b4
reb0rt081 <ribes.roberto@gmail.com>
2020-03-08 20:55:48 +0100

    Added new NerobScanControl

---
+++ Nerob.Client.Controls/NerobScannerControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nerob.Client.Controls
+{
+    /// <summary>
+    /// Interaction logic for ScannerControl.xaml
+    /// </summary>
+    public partial class NerobScannerControl : UserControl
+    {
+        public NerobScannerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
