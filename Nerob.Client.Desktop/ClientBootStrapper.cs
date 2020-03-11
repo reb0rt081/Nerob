2ccb7990e6c7e47d4f9b74ee0fd9a302f2a7294b
reb0rt081 <ribes.roberto@gmail.com>
2020-03-11 15:54:14 +0100

    Small changes

--- Nerob.Client.Desktop/ClientBootStrapper.cs
+++ Nerob.Client.Desktop/ClientBootStrapper.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-
-using CommonServiceLocator;
-using Nerob.Client.Modules.Picking;
 using Nerob.Client.Modules.Picking.Views;
 
 using Prism.Regions;
