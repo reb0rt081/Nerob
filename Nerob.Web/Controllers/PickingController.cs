fa794cb7f3092790d3beaf5c871746ac75b7406e
reb0rt081 <ribes.roberto@gmail.com>
2020-03-15 18:35:30 +0100

    Added Picking Controller for web

---
+++ Nerob.Web/Controllers/PickingController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Nerob.Web.Controllers
+{
+    public class PickingController : ApiController
+    {
+        // GET: api/Picking
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Picking/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Picking
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Picking/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Picking/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
